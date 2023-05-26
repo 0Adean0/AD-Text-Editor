50b80accac7b4e60e8597f28f6789fbf6e86a659
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-05-26 15:24:57 -0400

    adjustable changes to webpack and src-sw, moving to install and database

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,8 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(({request})=>["style","script","works"].includes(request.destination),
+new CacheableResponsePlugin({
+  statuses:[0,200],
+})
+);
--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,13 +15,42 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+    new InjectManifest({
+    swSrc:"./src-sw.js",
+    swDesti:"src-sw.js"
+    }),
+    new HtmlWebpackPlugin({
+      template:"./index.html",
+      title:"AD Text Editor"}),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject:true,
+      name: "AD Text Editor",
+      description: "Insert your text here",
+      start_url:"./",
+      publicPath:"./",
       
-    ],
-
+      icons:[{
+        src:path.resolve("src/images/logo.png"),
+        sizes:[100,130,190,250,370,500],
+        destination:path.join("assets","icons")
+      }]
+    })
+  ],
     module: {
-      rules: [
-        
-      ],
-    },
+      rules: [{
+      test:/\.css$/i,
+      use:["style-loader", "css-loader"]
+      },
+      {test:/\.m?js$/,
+      exclude:/node_modules/,
+      use:{
+        loader:'Babel-loader',
+        options:{
+          presets:["@babel/preset-env"],
+          plugin:["@babel/plugin-proposal-object-rest-spread","@babel/transform-runtime"]
+        }
+       }}],
+      },
   };
 };
